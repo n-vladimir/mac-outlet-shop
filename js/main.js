b527137e7cfddf30334a880b4bc812bd8506fc33
n-vladimir <80723647+n-vladimir@users.noreply.github.com>
2021-05-21 15:57:46 +0300

    add hw8

---
+++ js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    $('.single-item').slick();
+});
+
+const listHandlers = [
+    { btnSelector: '#btnInputFilter', containerSelector: '.filter__container', toggleClassName: 'active' },
+    { btnSelector: '#btnInputPrice', containerSelector: '.orderListBtnInputPrice', toggleClassName: 'active' },
+    { btnSelector: '.nameSelect', containerSelector: '.itemsSelect', toggleClassName: 'activeFilterName' },
+];
+function launchListHandlers(handlers) {
+    handlers.forEach((handler) => {
+        const btnElements = document.querySelectorAll(handler.btnSelector);
+        const containers = document.querySelectorAll(handler.containerSelector);
+        for (let i = 0; i < btnElements.length; i++) {
+            btnElements[i].onclick = function (e) {
+                e.preventDefault();
+                if (containers[i]) {
+                    containers[i].classList.toggle(handler.toggleClassName);
+                }
+            }
+        }
+    });
+}
+launchListHandlers(listHandlers);
+
+
+const renderCard = (item) => {
+    const divElemItem = document.createElement('div');
+    const divElemItemInfo = document.createElement('div');
+    const divElemItemAbout = document.createElement('div');
+    divElemItem.className = 'item';
+    divElemItemInfo.className = 'device_item';
+    divElemItemAbout.className = 'device_about';
+
+    divElemItemInfo.innerHTML = `
+        <i class="fas fa-heart like"></i>
+        <img class="imgDevice" src="img/${item.imgUrl}" alt="iphone11MAX">
+        <h2 class="nameDevice">${item.name}</h2>
+        <p class="availability"><img class="imgAvailability" src="img/icons/close.svg" alt="imgAvailability">${item.orderInfo.inStock} left in stock</p>
+        <p class="priceDevice"> Price: ${item.price} $</p>
+    `;
+
+    const btn = document.createElement('button');
+    btn.className = 'btnDevice';
+    btn.id = item.id;
+    btn.innerText = 'Add to cart';
+    divElemItemInfo.appendChild(btn);
+
+    divElemItemAbout.innerHTML = `
+        <img class="like" src="img/icons/like_filled_red.svg" alt="like">
+        <span class="reviews"><span class="procent">${item.orderInfo.reviews}%</span> Positive reviews Above avarage</span>
+        <span class="num">560<span class="ins"> orders</span></span>
+    `;
+    // divElemItemInfo.appendChild(btnLike);
+    divElemItem.appendChild(divElemItemInfo);
+    divElemItem.appendChild(divElemItemAbout);
+
+    return divElemItem;
+
+};
+
+const renderCards = (item) => {
+    const containerElem = document.querySelector('.device__items');
+    containerElem.innerHTML = '';
+    const arrOfCardElems = item.map(item => renderCard(item));
+    containerElem.append(...arrOfCardElems);
+};
+
+renderCards(items);
+
+
+
+
+
+
+
+
+
+
