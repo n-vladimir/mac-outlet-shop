94e5bf66f19c4d556564f7eb0d7d289ca6a530ca
n-vladimir <80723647+n-vladimir@users.noreply.github.com>
2021-05-26 18:58:46 +0300

    add hw9

--- js/main.js
+++ js/main.js
@@ -23,22 +23,8 @@ function launchListHandlers(handlers) {
 }
 launchListHandlers(listHandlers);
 
-
-//-----------------------modal
-
-const showModal = (data) => {
-  const modal = document.querySelector('.modal');
-
-};
-
-
-
-
-
-
-
-
-
+const inputSearch = document.querySelector('#inputSearch');
+const sortByPrice = document.querySelector('#sortByPrice');
 
 //-----------------renderCard
 /**
@@ -75,6 +61,9 @@ const renderCard = (item) => {
     btn.id = item.id;
     btn.innerText = 'Add to cart';
     if(item.orderInfo.inStock <= 0) btn.classList.add('disabled');
+    btn.onclick = function (event) {
+        showModal(item);
+    };
     divElemItemInfo.appendChild(btn);
 
     divElemItemAbout.innerHTML = `
@@ -82,7 +71,6 @@ const renderCard = (item) => {
         <span class="reviews"><span class="procent">${item.orderInfo.reviews}%</span> Positive reviews Above avarage</span>
         <span class="num">560<span class="ins"> orders</span></span>
     `;
-    // divElemItemInfo.appendChild(btnLike);
     divElemItem.appendChild(divElemItemInfo);
     divElemItem.appendChild(divElemItemAbout);
 
@@ -94,6 +82,10 @@ const renderCard = (item) => {
 const renderCards = (item) => {
     const containerElem = document.querySelector('.device__items');
     containerElem.innerHTML = '';
+    if(!item.length) {
+        containerElem.innerHTML = '<h1>Product not found</h1>';
+        return;
+    }
     const arrOfCardElems = item.map(item => renderCard(item));
     containerElem.append(...arrOfCardElems);
 };
@@ -107,6 +99,126 @@ btnLike.forEach((item) => {
         item.classList.toggle('like-full');
     }
 });
+
+
+//-----------------------modal
+
+const showModal = (item) => {
+    const modal = document.querySelector('.modal');
+    modal.onclick = (event) => {
+        if(modal === event.target) {
+            modal.classList.remove('active--modal');
+            document.body.style.overflow = '';
+        }
+
+    };
+    modal.classList.add('active--modal');
+    document.body.style.overflow = 'hidden';
+
+    modal.querySelector('.imgUnit').innerHTML = `
+        <img class="imgDevice" src="img/${item.imgUrl}" alt="device ${item.name}">
+    `;
+    modal.querySelector('.infoUnit').innerHTML = `
+        <h2 class="nameDevice">${item.name}</h2>
+        <div class="device_about">
+            <img class="like" src="img/icons/like_filled_red.svg" alt="like">
+            <span class="reviews"><span class="procent">50%</span> Positive reviews Above avarage</span>
+            <span class="num">560<span class="ins"> orders</span></span>
+        </div>
+        <ul class="infoItem">
+            <li><p>Color: <span>${item.color}</span></p></li>
+            <li><p>Operation System: <span>${item.os}</span></p></li>
+            <li><p>Chip: <span>${item.chip.name}</span></p></li>
+            ${
+        Object.entries(item.size).map(([key,value]) => {
+            return `<li>
+                         <p>${key[0].toUpperCase()+key.slice(1, key.length)}: <span>${value}</span>${(function () {
+                            if(key === 'weight'){
+                                return ' g';
+                            } else {
+                                return ' cm';
+                            }
+                            })()}</p>
+                    </li>`
+        }).join('')
+        }
+        </ul>
+    `;
+
+    modal.querySelector('.priceAndBtnUnit').innerHTML = `
+        <p class="priceDevice">$ ${item.price}</p>
+        <p>${item.orderInfo.inStock} left in stock</p>
+        <button class="btnDevice">Add to cart</button>
+    `;
+};
+
+
+
+/**
+ * -------------------------------render SORT and SEARCH
+ */
+class SortPriceAndSearch {
+    #items = [...items];
+    #options = {
+        find: '',
+        sort: 'def',
+    };
+    constructor() {}
+
+    set options(optionsA) {
+        Object.assign(this.#options, optionsA);
+        this.findByName();
+        this.sortByPrice();
+        renderCards(this.#items);
+    }
+    set itemsProduct(arr) {
+        this.#items = arr.map((item) => {
+            const newItem = {...item};
+            return newItem;
+        });
+
+    }
+
+    get itemsProduct() {
+        return [...this.#items];
+    }
+
+    findByName() {
+        sortAndSearch.itemsProduct = items.filter((item) => {
+            return item.name.toLowerCase().includes(this.#options.find.toLowerCase())
+        })
+    }
+    sortByPrice() {
+        if(this.#options.sort === 'def') {
+           this.#items = [...sortAndSearch.itemsProduct];
+           return;
+        }
+        if(this.#options.sort === 'asc') {
+            sortAndSearch.itemsProduct = sortAndSearch.itemsProduct.sort((a,b) => {
+                const first = a.price;
+                const second = b.price;
+                return first - second;
+            });
+            return;
+        }
+        sortAndSearch.itemsProduct = sortAndSearch.itemsProduct.sort((a,b) => {
+            const first = a.price;
+            const second = b.price;
+            return second - first;
+        })
+    }
+}
+
+const sortAndSearch = new SortPriceAndSearch();
+
+inputSearch.oninput = function () {
+  sortAndSearch.options = {find: this.value};
+};
+sortByPrice.onchange = function () {
+    sortAndSearch.options = {sort: this.value};
+};
+
+
 
 
 
